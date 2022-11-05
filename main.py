759182661cdee4a8070cb49f23914c727f1d7467
tebik44 <terbik123255@gmail.com>
2022-11-05 10:08:22 +0300

    добавл суперфункцию

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+def my_superfanction():
+    print("вообще то 101112")
 def main():
     print(123)
     print(456)
     print(789)
     print(228)
+    my_superfanction()
 
 if __name__ == '__main__':
     main()
